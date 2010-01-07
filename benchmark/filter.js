f8fc7698cdd401c868d9e2a98bb47ae3f0d82901
Felix Geisendörfer <felix@debuggable.com>
2010-01-08 00:21:59 +0100

    Benchmark for filtering dirty documents. 33 mio docs / sec
    Filtered 100000 docs in 3 ms	(33333333 per sec)
    
    MacBook Pro, OSX 10.6.2, 2.66 Ghz Intel Core 2 Duo, 4GB 1067 Mhz DDR3

---
+++ benchmark/filter.js
@@ -0,0 +1,25 @@
+process.mixin(require('sys'));
+var
+  Dirty = require('../lib/dirty').Dirty,
+  NUM = 100000,
+  posts = new Dirty('posts');
+
+for (var i = 0; i < NUM; i++) {
+  posts.set(i, {str: 'This is a 256 byte string. This is a 256 byte string. This is a 256 byte string. This is a 256 byte string. This is a 256 byte string. This is a 256 byte string. This is a 256 byte string. This is a 256 byte string. This is a 256 byte string. This is a 256'});
+}
+
+posts.addListener('flush', function() {
+  var
+    start = +new Date(),
+    i = 0;
+
+  posts.filter(function(doc) {
+    i++;
+  });
+
+  var
+    duration = (+new Date()) - start,
+    perSec = (i/duration*1000).toFixed(0);
+
+  puts('Filtered '+i+' docs in '+duration+' ms '+"\t"+'('+perSec+' per sec)');
+});
