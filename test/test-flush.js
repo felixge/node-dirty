931a8759c35f892f2b53290db4c15a3c98e1484f
Felix Geisendörfer <felix@debuggable.com>
2010-01-08 19:42:34 +0100

    Only emit flush event if all docs are on disk
    There was a bug causing the flush event to potentially emit while some
    documents were still in memory and not yet scheduled for flushing.

---
+++ test/test-flush.js
@@ -0,0 +1,25 @@
+process.mixin(require('./common'));
+
+var
+  FILE = path.join(path.dirname(__filename), 'flush.dirty'),
+
+  // Only emit flush if we're really in sync with the disc
+  EXPECTED_FLUSHES = 1,
+
+  db = new Dirty(FILE, {flushLimit: 2, flushInterval: null}),
+
+  timesFlushed = 0;
+
+db.add({}, function() {
+  db.add({});
+});
+db.add({});
+db.add({});
+
+db.addListener('flush', function() {
+  timesFlushed++;
+});
+
+process.addListener('exit', function() {
+  assert.equal(EXPECTED_FLUSHES, timesFlushed);
+});
