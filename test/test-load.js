589b80de50117a2336f30a516076dfcb7ee50bd9
Felix Geisendörfer <felix@debuggable.com>
2010-01-27 23:17:04 +0100

    Finished load test
    Also found a bug in remove(), fixed now.

--- test/test-load.js
+++ test/test-load.js
@@ -8,7 +8,9 @@ var
   TEST_ID2 = 'you-stay',
   TEST_DOC2 = {makeMe: 'happy'},
 
-  db1 = new Dirty(FILE);
+  db1 = new Dirty(FILE),
+
+  expectLoadedDocs = 1;
 
 db1.set(TEST_ID, TEST_DOC);
 db1.set(TEST_ID2, TEST_DOC2);
@@ -20,8 +22,15 @@ db1.addListener('flush', function() {
     .load()
     .addCallback(function() {
       posix.unlink(FILE);
+
       db2.filter(function(doc) {
         assert.ok(!doc._deleted);
+        assert.deepEqual(TEST_DOC2, doc);
+        expectLoadedDocs--;
       });
     });
+});
+
+process.addListener('exit', function() {
+  assert.equal(0, expectLoadedDocs);
 });
