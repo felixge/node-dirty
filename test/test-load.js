5eeb84598844087c405c514d4d9389abf5e21b7b
Felix Geisendörfer <felix@debuggable.com>
2010-01-27 22:58:09 +0100

    Test for load()
    Also serves as a test case for:
    
    http://github.com/felixge/node-dirty/issues#issue/2

---
+++ test/test-load.js
@@ -0,0 +1,27 @@
+process.mixin(require('./common'));
+
+var
+  FILE = path.join(path.dirname(__filename), 'load.dirty'),
+
+  TEST_ID = 'you-get-deleted',
+  TEST_DOC = {makesMe: 'sad'},
+  TEST_ID2 = 'you-stay',
+  TEST_DOC2 = {makeMe: 'happy'},
+
+  db1 = new Dirty(FILE);
+
+db1.set(TEST_ID, TEST_DOC);
+db1.set(TEST_ID2, TEST_DOC2);
+db1.remove(TEST_ID);
+
+db1.addListener('flush', function() {
+  var db2 = new Dirty(FILE);
+  db2
+    .load()
+    .addCallback(function() {
+      posix.unlink(FILE);
+      db2.filter(function(doc) {
+        assert.ok(!doc._deleted);
+      });
+    });
+});
