d4d23c1a037c26ca532df36f9734b843aed045ff
Mike McNeil <michael.r.mcneil@gmail.com>
2013-03-21 02:00:02 -0500

    Boilerplate mem/disk setup in new test.  Added test db file to gitignore.

--- test/node_v0.10.0.js
+++ test/node_v0.10.0.js
@@ -1,3 +1,5 @@
+var dirty = require('../index.js');
+
 if (!process.version.match(/v0\.10\./)) {
 	console.warn("WARNING: Known issue in node.js v0.10.x can only be tested with an appropriate version of Node installed.");
 }
@@ -5,10 +7,41 @@ else {
 	// Test Node v0.10.0 compatibility
 	describe('with node v0.10.0', function() {
 			describe('db.set()', function() {
-				it('should trigger the callback if provided', function() {
-					// TODO
-					return false;
+				describe('using the disk store', function() {
+					it('should trigger the callback if provided', function(cb) {
+						connectToMemDb(function (err, db) {
+							if (err) return cb(err);
+
+							cb();
+						});
+					});
+				});
+				describe('using the memory store', function() {
+					it('should trigger the callback if provided', function(cb) {
+						connectToDiskDb(function (err, db) {
+							if (err) return cb(err);
+
+							cb();
+						});
+					});
 				});
 			});
+	});
+}
+
+
+// Instantiate sample memory database
+function connectToMemDb(cb) {
+	var db = new(dirty.Dirty)();
+	db.on('load', function (err){
+		cb(err, db);
+	});
+}
+
+// Instantiate sample disk database
+function connectToDiskDb(cb) {
+	var db = dirty('dirty.db');
+	db.on('load', function (err){
+		cb(err, db);
 	});
 }
